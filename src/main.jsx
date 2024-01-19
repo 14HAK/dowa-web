afd2a72338163d8ada6183774fccda3b2a7a66e5
Dulon <dulonmolla88@gmail.com>
2024-01-19 17:10:40 +0600

    part 2 dowa kobul howar somoy ooo isthan

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+import './index.css';
+import Base from './base.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Base />
+    {/* <App /> */}
+  </React.StrictMode>
+);
