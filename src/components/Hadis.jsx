fa9e79e92a5f6b7793c293c05af8e454f5879c38
Dulon <dulonmolla88@gmail.com>
2024-02-17 23:07:40 +0600

    hadis add part-1,:27

--- src/components/Hadis.jsx
+++ src/components/Hadis.jsx
@@ -3,14 +3,41 @@ import data from '../data/hadisData';
 function Hadis() {
   return (
     <div>
-      {data.map((item) => (
-        <h2
-          key={item.slice(0, 1)}
-          className='text-xl p-2 text-left text-gray-600'
-        >
-          <span className='font-semibold'>{item.slice(0, 2)} </span>
-          {item.slice(3, item.length)}
-        </h2>
+      {data?.map((item) => (
+        <div key={item?.id}>
+          {item?.title ? (
+            <h1 className='font-arabic font-bold p-1 text-xl text-gray-800 text-right'>
+              {item?.title}
+            </h1>
+          ) : (
+            ' '
+          )}
+          {/* // */}
+          {/* {item?.notes ? (
+            <>
+              {item?.notes?.map((note) => (
+                <>
+                  <h2
+                    key={1}
+                    className='text-xl p-1 font-arabic text-left text-gray-600'
+                  >
+                    <span className='font-semibold'></span>
+                    {note}
+                  </h2>
+                </>
+              ))}
+            </>
+          ) : (
+            ' '
+          )} */}
+
+          <h2
+            key={1}
+            className='text-xl p-2 font-arabic text-left text-gray-600'
+          >
+            {item?.notes?.join(`___`)}
+          </h2>
+        </div>
       ))}
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
-// import Hadis from './components/Hadis.jsx';
+import Hadis from './components/Hadis.jsx';
 // import Base from './base.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Base /> */}
-    <App />
-    {/* <Hadis /> */}
+    {/* <App /> */}
+    <Hadis />
   </React.StrictMode>
 );
